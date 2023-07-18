7299d3387bc9ce11f24d6f3726048fd54f9ccb1c
ztanvir <zahirulopel@gmail.com>
2023-07-18 20:29:32 +0600

    Add solution for courseInfromation step1 exercise 1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,26 @@
+const Header = (props)=>{
+  return(
+    <h1>{props.courseName}</h1>
+  )
+}
+const Content = (props)=>{
+  return (
+    <>
+      <p>{props.contentPart1}{props.contentExercises1}</p>
+      <p>{props.contentPart2}{props.contentExercises2}</p>
+      <p>{props.contentPart3}{props.contentExercises3}</p>
+    </>
+  )
+}
+
+const Total = (props)=>{
+  return (
+    <>
+      <p>Number of exercises {props.courseExercise1 + props.courseExercise2 + props.courseExercise3}</p>
+    </>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +32,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header courseName={course}/>
+      <Content contentPart1={part1} contentExercises1={exercises1} contentPart2={part2} contentExercises2={exercises2} contentPart3={part3} contentExercises3={exercises3} />
+      <Total courseExercise1={exercises1} courseExercise2={exercises2} courseExercise3={exercises3}/>
     </div>
   )
 }
