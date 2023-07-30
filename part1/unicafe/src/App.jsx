e7dd6a8e52822fb1576817b23ac88aa488e86277
ztanvir <zahirulopel@gmail.com>
2023-07-30 11:52:07 +0600

    Add solution for unicafe step2 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,15 +10,46 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [totalFeedbacks, setTotalFeedbacks] = useState(0);
+
+  const averageScore = () => {
+    // good 1,neutral 0,bad -1
+    const goodPoints = 1;
+    const neutralPoints = 0;
+    const badPoints = -1;
+    if (totalFeedbacks === 0) {
+      return "";
+    }
+    return (
+      (good * goodPoints + neutralPoints * neutralPoints + bad * badPoints) /
+      totalFeedbacks
+    );
+  };
+
+  const positiveScore = () => {
+    if (totalFeedbacks === 0) {
+      return "";
+    }
+    return good / totalFeedbacks;
+  };
 
   const clickGoodBtn = () => {
     setGood(good + 1);
+    // total feedback
+    let addOne = 1;
+    setTotalFeedbacks(good + neutral + bad + addOne);
   };
   const clickNeutralBtn = () => {
     setNeutral(neutral + 1);
+    // total feedback
+    let addOne = 1;
+    setTotalFeedbacks(good + neutral + bad + addOne);
   };
   const clickBadBtn = () => {
     setBad(bad + 1);
+    // total feedback
+    let addOne = 1;
+    setTotalFeedbacks(good + neutral + bad + addOne);
   };
 
   return (
@@ -31,6 +62,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {totalFeedbacks}</p>
+      <p>average {averageScore()}</p>
+      <p>positive {positiveScore()} %</p>
     </div>
   );
 };
