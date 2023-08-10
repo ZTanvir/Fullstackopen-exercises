f1d4b8c5213243ee8afc426731494fb6ac4cad00
ztanvir <zahirulopel@gmail.com>
2023-08-10 12:59:42 +0600

    Add phonebook solution exercise-2.8 for part2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,54 +3,81 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newPhoneNumber, setNewPhoneNumber] = useState("");
 
-  // Add id to the persons
+  // Add phonenumber and id to the persons
   const updatePersons = persons.map((person) => {
     let uuid = self.crypto.randomUUID();
-    return { ...person, id: uuid };
+    return { ...person, number: "040-1234567", id: uuid };
   });
-  // Add name to the persons when sumit the form
+
+  // Add name and phone number to the persons when submit the form
   const addName = (event) => {
     event.preventDefault();
-    if (newName !== "") {
+    if (newName !== "" && newPhoneNumber !== "") {
       // check if the name is already present in updatePersons
-      const matchPerson = persons.some((person) => person.name === newName);
+      const matchPerson = updatePersons.some(
+        (person) => person.name === newName
+      );
       if (matchPerson) {
         alert(`${newName} is already added to phonebook`);
       } else {
         let uuid = self.crypto.randomUUID();
         const newPerson = {
           name: newName,
+          number: newPhoneNumber,
           id: uuid,
         };
         setPersons(updatePersons.concat(newPerson));
       }
       setNewName("");
+      setNewPhoneNumber("");
     }
   };
-  // Display phonebook person name
-  const displayPersonsName = (personList) => {
-    return personList.map((person) => {
-      return <p key={person.id}>{person.name}</p>;
-    });
+  // Display phonebook person name and number
+  const displayPersonsName = (personsList) => {
+    return personsList.map((person) => (
+      <p key={person.id}>
+        {person.name} {person.number}
+      </p>
+    ));
   };
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name:
-          <input
-            value={newName}
-            onChange={(event) => setNewName(event.target.value)}
-          />
+          <div>
+            <label>
+              name:
+              <input
+                type="text"
+                value={newName}
+                onChange={(event) => setNewName(event.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              number:
+              <input
+                type="text"
+                value={newPhoneNumber}
+                onChange={(event) => setNewPhoneNumber(event.target.value)}
+              />
+            </label>
+          </div>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {displayPersonsName(updatePersons)}
+      <div>
+        {persons.length === 1
+          ? displayPersonsName(updatePersons)
+          : displayPersonsName(persons)}
+      </div>
     </div>
   );
 };
