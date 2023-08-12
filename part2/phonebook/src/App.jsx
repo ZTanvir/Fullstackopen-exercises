19c5167d0649a28c65c0dc0084980c2f83da69ee
ztanvir <zahirulopel@gmail.com>
2023-08-12 12:56:03 +0600

    Add solution for exercise 2.10 part2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,84 @@
 import { useState } from "react";
 
+const Person = ({ personList, findName }) => {
+  // Add phonenumber and id to the persons
+  const updatePersons = personList.map((person) => {
+    let uuid = self.crypto.randomUUID();
+    return { ...person, number: "040-1234567", id: uuid };
+  });
+  // Display phonebook person name and number
+  const displayPersonsName = (personsList) => {
+    return personsList.map((person) => (
+      <p key={person.id}>
+        {person.name} {person.number}
+      </p>
+    ));
+  };
+  // Filter persons based on searchName
+  const filterPerson = personList.filter((person) => {
+    return person.name.toLocaleLowerCase().includes(findName.toLowerCase());
+  });
+  return (
+    <div>
+      {personList.length === 1
+        ? displayPersonsName(updatePersons)
+        : displayPersonsName(filterPerson)}
+    </div>
+  );
+};
+const PersonForm = ({
+  handleAddName,
+  addName,
+  setName,
+  addPhone,
+  setPhone,
+}) => {
+  return (
+    <form onSubmit={handleAddName}>
+      <div>
+        <div>
+          <label>
+            name:
+            <input
+              type="text"
+              value={addName}
+              onChange={(event) => setName(event.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            number:
+            <input
+              type="text"
+              value={addPhone}
+              onChange={(event) => setPhone(event.target.value)}
+            />
+          </label>
+        </div>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+const Filter = ({ findName, setFindName }) => {
+  return (
+    <form>
+      <label>
+        filter shown with
+        <input
+          type="text"
+          value={findName}
+          onChange={(event) => {
+            setFindName(event.target.value);
+          }}
+        />
+      </label>
+    </form>
+  );
+};
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
@@ -17,9 +96,7 @@ const App = () => {
     event.preventDefault();
     if (newName !== "" && newPhoneNumber !== "") {
       // check if the name is already present in updatePersons
-      const matchPerson = updatePersons.some(
-        (person) => person.name === newName
-      );
+      const matchPerson = persons.some((person) => person.name === newName);
       if (matchPerson) {
         alert(`${newName} is already added to phonebook`);
       } else {
@@ -35,68 +112,21 @@ const App = () => {
       setNewPhoneNumber("");
     }
   };
-  // Display phonebook person name and number
-  const displayPersonsName = (personsList) => {
-    return personsList.map((person) => (
-      <p key={person.id}>
-        {person.name} {person.number}
-      </p>
-    ));
-  };
-  // Filter persons based on searchName
-  const filterPerson = persons.filter((person) => {
-    return person.name.toLocaleLowerCase().includes(searchName.toLowerCase());
-  });
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <label>
-          filter shown with
-          <input
-            type="text"
-            value={searchName}
-            onChange={(event) => {
-              setSearchName(event.target.value);
-              console.log(searchName);
-            }}
-          />
-        </label>
-      </form>
-      <h2>add a new</h2>
-      <form onSubmit={addName}>
-        <div>
-          <div>
-            <label>
-              name:
-              <input
-                type="text"
-                value={newName}
-                onChange={(event) => setNewName(event.target.value)}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              number:
-              <input
-                type="text"
-                value={newPhoneNumber}
-                onChange={(event) => setNewPhoneNumber(event.target.value)}
-              />
-            </label>
-          </div>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>
-        {persons.length === 1
-          ? displayPersonsName(updatePersons)
-          : displayPersonsName(filterPerson)}
-      </div>
+      <Filter findName={searchName} setFindName={setSearchName} />
+      <h3>add a new</h3>
+      <PersonForm
+        handleAddName={addName}
+        addName={newName}
+        setName={setNewName}
+        addPhone={newPhoneNumber}
+        setPhone={setNewPhoneNumber}
+      />
+      <h3>Numbers</h3>
+      <Person personList={persons} findName={searchName} />
     </div>
   );
 };
