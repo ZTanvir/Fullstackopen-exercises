4cb889e5ed339044a9a9e5a1753d8233d890a506
ztanvir <zahirulopel@gmail.com>
2023-08-21 12:15:22 +0600

    Add solution for exercise 2.13 part 2

---
+++ part2/phonebook/src/services/personsService.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3000/persons";
+
+const getAll = () => {
+  const response = axios.get(baseUrl);
+  return response.then((reply) => reply.data);
+};
+
+const create = (newObject) => {
+  const response = axios.post(baseUrl, newObject);
+  return response.then((reply) => reply.data);
+};
+
+export default {
+  getAll,
+  create,
+};
