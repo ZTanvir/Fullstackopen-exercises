30b32d070bf1999c28867e5a37f66849d0616754
ztanvir <zahirulopel@gmail.com>
2023-08-21 12:37:48 +0600

    Add solution for exercise 2.13 part 2

--- part2/phonebook/src/services/personsService.js
+++ part2/phonebook/src/services/personsService.js
@@ -11,7 +11,13 @@ const create = (newObject) => {
   return response.then((reply) => reply.data);
 };
 
+// const deleteData = (id) => {
+//   const resourceItemUrl = `http://localhost:3000/persons/${id}`;
+//   const response = axios.delete(resourceItemUrl);
+//   return response.then((reply) => reply.data);
+// };
 export default {
   getAll,
   create,
+  //   deleteData,
 };
