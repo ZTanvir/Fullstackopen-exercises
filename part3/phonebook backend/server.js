d63d9af7099a913ff3a931a46384e4cee3f44080
ztanvir <zahirulopel@gmail.com>
2023-09-11 09:51:51 +0600

    Add solution for exercise 3.7 part 3

--- part3/phonebook backend/server.js
+++ part3/phonebook backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+// import morgan middleware
+const morgan = require("morgan");
+app.use(morgan("tiny"));
 app.use(express.json());
 // Date today
 const today = new Date();
