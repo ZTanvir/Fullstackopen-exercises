292fdaec32caca6fc414d32981a9e0d2346454e5
ztanvir <zahirulopel@gmail.com>
2023-10-05 10:54:14 +0600

    Add solution for exercise 4.4 step 2

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blog) => {
   return 1;
 };
 
+const totalLikes = (blogPostList) => {
+  const totalLikes = blogPostList.reduce((acc, cur) => acc + cur.likes, 0);
+
+  return blogPostList.length === 0 ? 0 : totalLikes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
