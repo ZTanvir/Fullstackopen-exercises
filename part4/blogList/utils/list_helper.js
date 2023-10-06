4c9546a19e7cbd4198ac2d75b18ed7b340a64524
ztanvir <zahirulopel@gmail.com>
2023-10-06 10:26:43 +0600

    Add solution for exercise 4.6 step4, part 4

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -20,8 +20,37 @@ const favoriteBlog = (blogList) => {
   return favoriteBlog;
 };
 
+const mostBlogs = (blogList) => {
+  let authorList = [];
+  let authorBlogs = {};
+  // store all the author from blog in author list
+  blogList.forEach((blog) => {
+    let author = blog.author;
+    if (!authorList.includes(author)) {
+      authorList = authorList.concat(author);
+    }
+  });
+  let blogs = 0;
+  //loop the authorlist and find how many blog they have written
+  authorList.forEach((blogWriter) => {
+    let writer = blogWriter;
+    let totalBlog = 0;
+    blogList.forEach((blog) => {
+      if (blog.author === writer) {
+        totalBlog += 1;
+      }
+      if (totalBlog > blogs) {
+        blogs = totalBlog;
+        authorBlogs = { ...authorBlogs, author: writer, blogs: totalBlog };
+      }
+    });
+  });
+  return blogList.length === 0 ? null : authorBlogs;
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
