619a49f9e2207dc96e2a0a300171a28f025a4668
ztanvir <zahirulopel@gmail.com>
2023-12-31 18:37:12 +0600

    Add solution for exercise part 5.23 step 7,part 5

--- part5/bloglist-frontend/cypress/e2e/BlogE2E/BlogApp.cy.js
+++ part5/bloglist-frontend/cypress/e2e/BlogE2E/BlogApp.cy.js
@@ -14,13 +14,13 @@ describe("Blog app", function () {
     });
     cy.visit("http://localhost:5173/");
   });
-  it.skip("Login form is shown", function () {
+  it("Login form is shown", function () {
     cy.contains("log in to application");
     cy.contains("Username");
     cy.contains("Password");
     cy.contains("Login");
   });
-  describe.skip("Login", function () {
+  describe("Login", function () {
     it("succeeds with correct credentials", function () {
       cy.get("#user").type("testuser");
       cy.get("#password").type("test12345");
@@ -65,6 +65,55 @@ describe("Blog app", function () {
       cy.get(".blog-title").parent().find("button").as("blogDetails");
       cy.get("@blogDetails").click();
       cy.contains("remove").should("not.exist");
+    });
+  });
+  describe("Exist several blog", function () {
+    const blogs = [
+      {
+        title: "blog1",
+        author: "testUser",
+        url: "https://www.google.com/",
+        user: "test",
+      },
+      {
+        title: "blog2",
+        author: "testUser",
+        url: "https://www.google.com/",
+        user: "test",
+      },
+      {
+        title: "blog3",
+        author: "testUser",
+        url: "https://www.google.com/",
+        user: "test",
+      },
+    ];
+    beforeEach(function () {
+      cy.login({ username: "testuser", password: "test12345" });
+      cy.createBlog(blogs[0]);
+      cy.createBlog(blogs[1]);
+      cy.createBlog(blogs[2]);
+      cy.get(".blog-title").parent().find("button").as("blogDetails");
+      cy.get("@blogDetails").click({ multiple: true });
+    });
+    it("Most like blog appear first in the list", function () {
+      cy.contains("blog1").parent().contains("like").as("blog1LikeBtn");
+      cy.contains("blog2").parent().contains("like").as("blog2LikeBtn");
+      cy.contains("blog3").parent().contains("like").as("blog3LikeBtn");
+
+      cy.get("@blog1LikeBtn").click();
+      cy.get("@blog2LikeBtn").click();
+      cy.get("@blog3LikeBtn").click();
+
+      cy.get("@blog2LikeBtn").click();
+      cy.get("@blog3LikeBtn").click();
+
+      cy.get("@blog3LikeBtn").click();
+
+      cy.visit("http://localhost:5173/");
+      cy.get(".blog").eq(0).should("contain", blogs[2].title);
+      cy.get(".blog").eq(1).should("contain", blogs[1].title);
+      cy.get(".blog").eq(2).should("contain", blogs[0].title);
     });
   });
 });
